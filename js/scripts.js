5fe88ded4e09a6838ab5887bb2f82881efb93851
inertial.observer <inertial.observer@gmail.com>
2015-07-02 12:48:46 -0700

    change html and variable names

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 //Pizza object with priceIt method
 var pizza = {
   priceIt: function() {
-    return 1 + (this.pizza_size * 1)
-             + (this.crust * 1)
-             + (this.meat_toppings * 1)
-             + (this.cheese * 1)
-             + (this.sauce + 0)
-             + (this.veggie_toppings * 1)
-             + (this.pizza_quantity * 1)
+    return 10 + (this.topping * 2)
+              + (this.pizza_size * 3)
+              + (this.crust * 3)
   }
 };
 
@@ -16,31 +12,33 @@ $(document).ready(function() {
   $('#order-form').submit(function(event){
     event.preventDefault();
 
-    //sets size selection
-    var select_size = parseInt($('#select_size').val());
-
-    //sets crust selection
-    var select_crust = parseInt($('#select_crust').val());
-
-    //sets sauce selection
-    var select_sauce = parseInt($('#select_sauce').val());
-
-    //sets cheese selection
-    var extra_cheese = parseInt($('#extra_cheese').val());
-
-    //adds checked meat toppings
-    var meat_topping_multiplier = 0;
+    //adds checked toppings to multiplier
+    var toppings_multiplier = 0;
     $(':checkbox:checked.topping-selection').each(function() {
-      meat_topping_multiplier += +this.value;
-    })
+      toppings_multiplier += +this.value;
+    });
+
+    // SETS SIZE SELECTION TO USER INPUT
+      var size_selection = parseInt($('#size-selection').val());
+
+      // SETS CRUST SELECTION TO USER INPUT
+      var crust_selection = parseInt($('#crust-selection').val());
 
     var pizza_order = Object.create(pizza);
 
-    pizza_order.pizza_size = select_size;
-    pizza_order.crust = select_crust;
-    pizza_order.meat_toppings = meat_topping_multiplier;
-    pizza_order.cheese = extra_cheese;
-    pizza_order.sauce = select_sauce;
+    pizza_order.topping = toppings_multiplier;
+    pizza_order.pizza_size = size_selection;
+    pizza_order.crust = crust_selection;
 
-  })
-})
+    var pizza_price = pizza_order.priceIt();
+    $('#pizza-price').show();
+
+    $('#order-price').text(pizza_price);
+
+    //unhighlights button when not hover
+    $(".btn").mouseup(function() {
+      $(this).blur();
+    });
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,53 @@
 describe('pizza', function() {
     describe('priceIt', function() {
-        it('will return a price of 1 for a topping with multiplier 0, size multiplier 0, crust multiplier 0', function() {
+        it('will return a price of 10 for a topping with multiplier 0, size multiplier 0, crust multiplier 0', function() {
             var testPizza = Object.create(pizza);
+            testPizza.topping = 0;
             testPizza.pizza_size = 0;
             testPizza.crust = 0;
-            testPizza.meat_toppings = 0;
-            testPizza.cheese = 0;
-            testPizza.sauce = 0;
-            testPizza.veggie_toppings = 0;
-            testPizza.pizza_quantity = 0;
-            expect(testPizza.priceIt()).to.equal(1);
+            expect(testPizza.priceIt()).to.equal(10);
         });
-      });
+        it('will return a price of 12 a topping with multiplier 1, size multiplier 0, crust multiplier 0', function() {
+            var testPizza = Object.create(pizza);
+            testPizza.topping = 1;
+            testPizza.pizza_size = 0;
+            testPizza.crust = 0;
+            expect(testPizza.priceIt()).to.equal(12);
+        });
+        it('will return a price of 12 a topping with multiplier -1, size multiplier 0, crust multiplier 0', function() {
+            var testPizza = Object.create(pizza);
+            testPizza.topping = -1;
+            testPizza.pizza_size = 0;
+            testPizza.crust = 0;
+            expect(testPizza.priceIt()).to.equal(8);
+        });
+        it('will return a price of 13 for a topping with multiplier 0, size multiplier 1, crust multiplier 0', function() {
+            var testPizza = Object.create(pizza);
+            testPizza.topping = 0;
+            testPizza.pizza_size = 1;
+            testPizza.crust = 0;
+            expect(testPizza.priceIt()).to.equal(13);
+        });
+        it('will return a price of 15 for a topping with multiplier 1, size multiplier 1, crust multiplier 0', function() {
+            var testPizza = Object.create(pizza);
+            testPizza.topping = 1;
+            testPizza.pizza_size = 1;
+            testPizza.crust = 0;
+            expect(testPizza.priceIt()).to.equal(15);
+        });
+        it('will return a price of 16 for a topping with multiplier 0, size multiplier 0, crust multiplier 1', function() {
+            var testPizza = Object.create(pizza);
+            testPizza.topping = 0;
+            testPizza.pizza_size = 0;
+            testPizza.crust = 1;
+            expect(testPizza.priceIt()).to.equal(13);
+        });
+        it('will return a price of 16 for a topping with multiplier 1, size multiplier 1, crust multiplier 1', function() {
+            var testPizza = Object.create(pizza);
+            testPizza.topping = 1;
+            testPizza.pizza_size = 1;
+            testPizza.crust = 1;
+            expect(testPizza.priceIt()).to.equal(18);
+        });
     });
+});
